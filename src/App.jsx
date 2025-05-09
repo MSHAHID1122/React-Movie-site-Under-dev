5eee24398f3224c8b9dc6e1e894eb2d082c2f21e
MSHAHID1122 <samd13894@gmail.com>
2025-05-09 20:23:23 +0500

    added state for user movie search

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import MovieCard from './components/MovieCard'
 import poster from './assets/react.svg'
+import Home from './pages/Home'
 
 function Test({text}){
     return (
@@ -15,13 +16,11 @@ function Test({text}){
     )
   }
 function App() {
-  const movieNumber =2;
+
   
   return (
     <>
-    {movieNumber=== 1 ?(<MovieCard movies={{title:'jangni',release_date:'12,1,2006',poster:poster}}/>):
-    (<MovieCard movies={{title:'seta',release_date:'12,1,2006',poster:poster}}/>)}
-    
+    <Home/>
     </>
      
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,42 @@
 import React from 'react'
+import MovieCard from '../components/MovieCard'
+import { useState } from 'react'
+function Home() {
+   const [user_input,set_input] = useState('');
 
-export const Home = () => {
+
+  const my_movies=[
+    {id:1,title:'movie1',release_date:'1,2,2203'},
+    {id:2,title:'movie2',release_date:'2,2,2203'},
+    {id:3,title:'movie3',release_date:'3,2,2203'},
+    {id:4,title:'movie4',release_date:'4,2,2203'}
+
+  ];
+  const handleSearch=(e)=>{
+    e.preventDefault()
+       alert(user_input)
+
+
+  }
+
+
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <div>
+        <form onSubmit={handleSearch}>
+         <input type='text ' placeholder='enter your text' value={user_input} onChange={(e)=>{set_input(e.target.value)}}></input>
+       <button type='submit'>Search</button>
+        </form>
+
+      </div>
+      <div>
+      { my_movies.map((movie)=>(
+        movie.title.toLowerCase().startsWith(user_input) &&
+        <MovieCard movies={movie} key={movie.id}/> ))
+    
+      }
+      </div>
+    </div> 
+    )
+  }
+export default Home
